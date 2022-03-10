0db36b47a1daf2c98a34e0f15a26673c0081cd76
VianoGuglielmo <100919429+VianoGuglielmo@users.noreply.github.com>
2022-03-10 16:31:18 +0100

    fatto a casa

--- Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
@@ -1,7 +1,10 @@
+/**
+ * Sample Skeleton for 'Scene.fxml' Controller Class
+ */
+
 package it.polito.tdp.parole;
 
-import it.polito.tdp.parole.model.Parole;
-
+import it.polito.tdp.parole.model.Parole; //importo la classe Parole così posso usare i metodi creati
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -11,44 +14,78 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class FXMLController {
-	
-	Parole elenco ;
 
-    @FXML
+	Parole elenco;
+	
+    @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
 
-    @FXML
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
 
-    @FXML
-    private TextField txtParola;
+    @FXML // fx:id="btnCancella"
+    private Button btnCancella; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnInserisci"
+    private Button btnInserisci; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnReset"
+    private Button btnReset; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtParola"
+    private TextField txtParola; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtResult"
+    private TextArea txtResult; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtTempo"
+    private TextArea txtTempo; // Value injected by FXMLLoader
 
     @FXML
-    private Button btnInserisci;
+    void doCancella(ActionEvent event) {
 
-    @FXML
-    private TextArea txtResult;
-
-    @FXML
-    private Button btnReset;
+    }
 
     @FXML
     void doInsert(ActionEvent event) {
-    	// TODO
+    	long start, end;
+    	String tem;
+    	start = System.nanoTime();
+    	String p = txtParola.getText(); 
+    	elenco.addParola(p);
+    	String ris = ""; // creo stringa vuota
+    	for (String s: elenco.getElenco()) { //for dove passo stringa e itera e assume i vari valori
+    		ris+= s+"\n";   // mi cambia la stringa precedente + quella nuova
+    	}
+    	end = System.nanoTime();
+    	txtParola.clear();
+    	txtResult.setText(ris);
+    	tem=String.valueOf(end-start);
+    	txtTempo.setText("Tempo impiegato aggiungere e riordinare parola: "+tem+ " nanosec");
     }
 
     @FXML
     void doReset(ActionEvent event) {
-    	// TODO
+    	long start, end;
+    	String tem;
+    	start = System.nanoTime();
+    	elenco.reset(); //metodo creato in parole usato per cancellare la lista
+    	txtResult.clear();
+    	txtTempo.clear();
+    	end = System.nanoTime();
+    	tem=String.valueOf(end-start);
+    	txtTempo.setText("Tempo impiegato eliminare testo e oul txt: "+tem+ " nanosec");
     }
 
-    @FXML
+    @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
-        assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnCancella != null : "fx:id=\"btnCancella\" was not injected: check your FXML file 'Scene.fxml'.";
         assert btnInserisci != null : "fx:id=\"btnInserisci\" was not injected: check your FXML file 'Scene.fxml'.";
-        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
         assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
-
-        elenco = new Parole() ;
+        assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtTempo != null : "fx:id=\"txtTempo\" was not injected: check your FXML file 'Scene.fxml'.";
+        elenco = new Parole();
     }
+
 }
--- Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
@@ -1,24 +1,35 @@
 package it.polito.tdp.parole.model;
 
 import java.util.List;
+import java.util.Collections;
+import java.util.LinkedList;
 
 public class Parole {
-		
+	
+	List<String> parole;
+	
+	
 	public Parole() {
-		//TODO
+		parole = new LinkedList <String>(); // inizializzo stringa	
 	}
 	
-	public void addParola(String p) {
-		//TODO
+	public void addParola(String p) { //metodo creato per aggiungere le parole alla lista
+		parole.add(p); 
 	}
 	
 	public List<String> getElenco() {
-		//TODO
-		return null;
-	}
+		List<String> lalfa = new LinkedList <String> (this.parole); // crea nuova lista temporanea dove al costruttore gli passo
+																	// la lista parole
+		Collections.sort(lalfa); //riordina in modo naturale con metodo compare quindi alfabetico
+		return lalfa;	
+	}																
 	
 	public void reset() {
-		// TODO
+		parole.clear(); // svuota la lista
 	}
-
+	
+	public void cancella() {
+		
+		
+	}
 }
